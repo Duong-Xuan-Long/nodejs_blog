59f87cf7ab69fda52db6f2d2b37d712f0644424e
Duong-Xuan-Long <linesco218@gmail.com>
2022-09-19 18:49:26 +0700

    g

--- src/index.js
+++ src/index.js
@@ -9,20 +9,18 @@ const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-    
 app.use(
-
-       express.urlencoded({
-            extended:true  ,
-      }),
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 //HTTP logger
  app.use(morgan('combined'));
 
 //Template engine
-      app.engine(
-    'hbs',
+ app.engine(
+     'hbs',
 
     handlebars.engine({
         defaultLayout: 'main',
